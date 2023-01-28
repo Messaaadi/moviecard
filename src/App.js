a15f9607c744fb511405ffffc02c25756c3e4f7b
messaadious <medmsoux156@gmail.com>
2023-01-28 22:52:32 +0100

    moviecard

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+import Filter from './Components/Filter';
+import MovieList from './Components/MovieList';
+import { movieData } from './Data';
+
+function App() {
+  const [movies, setMovies] = useState(movieData);
+  const [filterTitle, setFilterTitle] = useState("");
+  const [filterRating, setFilterRating] = useState(0)
+
+  return (
+    <div className="App">
+    <Filter setFilterTitle = {setFilterTitle} filterRating={filterRating} setFilterRating={setFilterRating} />
+    <MovieList movies={movies} setMovies={setMovies} filterTitle={filterTitle} filterRating={filterRating}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,23 @@
+export const movieData = [
+    {
+        id: Math.random(),
+        title: "The Godfather (1972)",
+        description:"The Godfather is set in the 1940s and takes place entirely within the world of the Corleones, a fictional New York Mafia family.",
+        posterUrl:"https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+        rating: 5
+    },
+    {
+        id: Math.random(),
+        title: "The Lord of the Rings Return of the King (2003)",
+        description:"The final confrontation between the forces of good and evil fighting for control of the future of Middle-earth.",
+        posterUrl:"https://m.media-amazon.com/images/I/71X6YzwV0gL.jpg",
+        rating: 4
+    },
+    {
+        id: Math.random(),
+        title: "Pulp Fiction (1994)",
+        description:" a genre of racy, action-based stories published in cheaply printed magazines from around 1900 to the 1950s, mostly in the United States.",
+        posterUrl:"https://cdn.europosters.eu/image/750/posters/pulp-fiction-cover-i1288.jpg",
+        rating: 3.5
+    },
+]
